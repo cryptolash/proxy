990169d5dc8294c32ae3504da7f43dac6f3538fb
cryptolash <107434542+cryptolash@users.noreply.github.com>
2023-06-02 23:29:56 -0400

    Update server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ var checkRateLimit = require('./lib/rate-limit')(process.env.CORSANYWHERE_RATELI
 var cors_proxy = require('./lib/cors-anywhere');
 cors_proxy.createServer({
   originBlacklist: originBlacklist,
-  originWhitelist: ['https://deportestvhd.com,https://talwan.makeup'],
+  originWhitelist: ['https://deportestvhd.com','https://talwan.makeup'],
   requireHeader: ['origin', 'x-requested-with'],
   checkRateLimit: checkRateLimit,
   removeHeaders: [
